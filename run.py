f5c959c307b9a3809d725e4fc5ffecd27ef440c3
Hedris Pereira <hedris.pereira@eemovel.com>
2024-10-02 08:48:41 -0300

    adjust run

--- run.py
+++ run.py
@@ -2,8 +2,8 @@ import os
 from src import create_app
 
 
-
+os.environ['production']
 
 if __name__ == "__main__":
     app = create_app()  
-    app.run(host=app.config['IP_HOST'], port=7500, debug=True)
+    app.run(host=app.config['IP_HOST'], port=app.config['PORT'], debug=app.config['DEGUG'])
