36d88ce76fac73e37dc0c813a704d48b1b0fe482
Hedris Pereira <hedris.pereira@eemovel.com>
2024-10-02 11:28:37 -0300

    changes

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from src import create_app
 
 if __name__ == "__main__":
     app = create_app()  
-    app.run(host=app.config['IP_HOST'], port=7500, debug=True)
+    app.run(host=app.config['IP_HOST'], port=app.config['PORT'], debug=app.config['DEBUG'])
