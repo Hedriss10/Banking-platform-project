191b170041f58cb572537295d2811b912e72fe40
Hedris Pereira <hedris.pereira@eemovel.com>
2025-01-25 19:39:36 -0300

    changes: add config project

--- src/utils/log.py
+++ src/utils/log.py
@@ -8,6 +8,19 @@ class DbLogger:
 
 
 def setup_logger(name: str):
+    """
+    Sets up a logger with the specified name. The logger outputs to both
+    the console and a file named 'src.log'. Console logs are set to INFO level,
+    while file logs are set to DEBUG level. The logs are formatted to include
+    the timestamp, logger name, log level, and message.
+
+    Args:
+        name (str): The name of the logger.
+
+    Returns:
+        logging.Logger: Configured logger instance.
+    """
+
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
 
